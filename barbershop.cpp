9a85db5aa92bd8c132c07318041fc291117d639d
swgoof <swgoof@admin.com>
2011-07-25 07:24:56 -0500

    updated code

--- barbershop.cpp
+++ barbershop.cpp
@@ -12,7 +12,6 @@
  */
 barbershop::barbershop() {
     pthread_mutex_init(&this->mutex, NULL);
-    pthread_cond_init(&this->cond_shop, NULL);
     pthread_cond_init(&this->cond_cust, NULL);
 }
 
@@ -23,64 +22,98 @@ barbershop::barbershop() {
  * sets shop_state to open.
  * A lock was obtained so that no other method could manipulate the data.
  */
-void barbershop::open(int num_chairs) {
+void barbershop::open(int num_chairs, int num_barbers) {
     // only perform when shop is closed
     if (!this->shop_state) {
         pthread_mutex_lock(&mutex);
+        // set up the conditional variables
+        this->NUM_BARBERS = num_barbers;
+        this->cond_barber = new pthread_cond_t[this->NUM_BARBERS];
+        this->barber_tosleep = new time_t[this->NUM_BARBERS];
+        this->barber_state = new status[this->NUM_BARBERS];
+        for (int i = 0; i < this->NUM_BARBERS; i++) {
+            pthread_cond_init(&this->cond_barber[i], NULL);
+            this->barber_tosleep[i] = clock();  //init sleep time
+            this->barber_state[i] = ASLEEP;
+        }
+        
+        
         this->MAX_CHAIRS = num_chairs;
-        this->barber_state = this->BARBER_ASLEEP;
         this->shop_state = true;
         pthread_mutex_unlock(&mutex);
         cout << "Welcome to the Barber shop. Today we have " << num_chairs
                 << " seats" << endl;
-        
-        // call a method to perform cuts
-        this->perform_cuts();
     }
 }
 
 /**
  * This method will close the barbershop
- * It sends out 2 signals for condition varibables on cond_shop and
- * cond_cust.
+ * It sends out signals for condition varibable cond_barber
  */
 void barbershop::close() {
     pthread_mutex_lock(&mutex);
     cout << " closing shop" << endl;
-    if (this->chairs.size() <= 0) {
-        pthread_cond_signal(&cond_shop);
-    }
     this->shop_state = false;
+    if (this->chairs.size() <= 0) {
+        for(int i = 0; i < this->NUM_BARBERS; i++) {
+            pthread_cond_signal(&this->cond_barber[i]);
+        }
+    } else {
+        cout << "Error: there are still customers" << endl;
+    }
     pthread_mutex_unlock(&mutex);
     cout << "Barber Shop is closed for the day(not accepting new customers)."
             << endl;
 }
 
+
+/**
+ * Cleanup, delete unneeded variables and such.
+ */
+void barbershop::cleanup() {
+    for(int i = 0; i < this->NUM_BARBERS; i++) {
+        pthread_cond_destroy(&this->cond_barber[i]);
+    }
+    pthread_cond_destroy(&this->cond_cust);
+    pthread_mutex_destroy(&this->mutex);
+    delete[] this->cond_barber;
+    delete[] this->barber_state;
+    delete[] this->barber_tosleep;
+    
+}
+
 /**
  * This is the method to perform the hair cuts
  */
-void barbershop::perform_cuts() {
+void barbershop::perform_cuts(int barber_id) {
     
     // continuous loop while shop is open
     while( this->shop_state ) {
         pthread_mutex_lock(&mutex);
         if (this->chairs.size() > 0) {
             // pop off queue
-            int id = this->chairs.front();  // get element
-            this->chairs.pop();     // remove element            
-            this->barber_state = this->BARBER_BUSY;
+            int cust_id = this->chairs.front();  // get element
+            this->chairs.pop();     // remove element
+            this->barber_state[barber_id] = BUSY;
             
+            pthread_mutex_unlock(&mutex);
             // random wait time for hair cut
             int r = rand();
             int wait = r % this->MAX_HAIR_TIME;
             usleep(wait);    // sleep for microseconds
-            cout << "  customer " << id << " is finished with hair cut: "
-                    << wait << endl;
+            pthread_mutex_lock(&mutex);
+            
+            cout << "  customer " << cust_id << " is finished with hair cut "
+                    << "from barber " << barber_id << endl;
             pthread_cond_signal(&this->cond_cust);
         } else {
-            cout << " Barber going to sleep, no customers." << endl;
-            this->barber_state = this->BARBER_ASLEEP;           
-            pthread_cond_wait(&cond_shop, &mutex);  //wait on shop
+            cout << " Barber " << barber_id << " going to sleep, no customers."
+                    << endl;
+            this->barber_state[barber_id] = ASLEEP;
+            // set the time Barber went to sleep
+            this->barber_tosleep[barber_id] = clock();
+            pthread_cond_wait(&this->cond_barber[barber_id], &mutex);  //wait on specific barber
+            this->barber_state[barber_id] = AWAKE;
         }
         pthread_mutex_unlock(&mutex);
     }
@@ -92,34 +125,49 @@ void barbershop::perform_cuts() {
  *
  * A hair cut should take a random amount of time less than MAX_HAIR_TIME
  */
-bool barbershop::request_cut(int id) {
+bool barbershop::request_cut(int cust_id) {
     bool got_in = false;
     pthread_mutex_lock(&mutex);
-    cout << "  customer " << id << " enters the shop." << endl;
+    cout << "  customer " << cust_id << " enters the shop." << endl;
     // only proceed if shop is open
     if (this->shop_state) {
         if (this->chairs.size() < this->MAX_CHAIRS) {
-            // check if barber is sleeping
-            if (this->barber_state == this->BARBER_ASLEEP) {
-                // wake up the barber
-                cout << "  customer " << id << " wakes up the Barber" << endl;
-                this->barber_state = this->BARBER_AWAKE;
+            // check if a barber is sleeping
+            // wake up the barber who has slept the longest
+            int max_bID = 0;    // barber that has been asleep the longest
+            double max_sleeptime = 0.0;
+            time_t now;
+            time(&now);
+            bool asleep = false;
+            for(int i = 0; i < this->NUM_BARBERS; i++) {
+                if (this->barber_state[i] == ASLEEP) {
+                    asleep = true;
+                    // find sleep time for each barber
+                    double diff = difftime(now, this->barber_tosleep[i]);
+                    if (diff > max_sleeptime) {
+                        max_sleeptime = diff;
+                        max_bID = i;
+                    }
+                }
+            }
+            if (asleep) {
+                cout << "  customer " << cust_id << " wakes up Barber "
+                        << max_bID << endl;
                 // signal barber
-                pthread_cond_signal(&cond_shop);
+                pthread_cond_signal(&this->cond_barber[max_bID]);
             }
             
             // add customer to the queue of chairs
-            this->chairs.push(id);
+            this->chairs.push(cust_id);
             pthread_cond_wait(&this->cond_cust, &mutex);
             got_in = true;
         } else {
-            cout << "  customer " << id << " is rejected (shop is full)." << endl;
+            cout << "  customer " << cust_id << " is rejected (shop is full)." << endl;
         }
         
     } else {
-        cout <<  "  customer " << id << " is rejected (shop is closed)." << endl;
+        cout <<  "  customer " << cust_id << " is rejected (shop is closed)." << endl;
     }
     pthread_mutex_unlock(&mutex);
     return got_in;
 }
-
--- barbershop.h
+++ barbershop.h
@@ -6,6 +6,7 @@
 //
 #include <iostream>
 #include <queue>
+#include <time.h>
 #include <pthread.h>
 using namespace std;
 
@@ -14,30 +15,31 @@ using namespace std;
 
 #endif	/* _BARBERSHOP_H */
 
+enum status {ASLEEP, BUSY, AWAKE};
+
 class barbershop {
 private:
-    const static int MAX_HAIR_TIME = 60000;
-    const static int BARBER_ASLEEP = 0;
-    const static int BARBER_BUSY = 1;
-    const static int BARBER_AWAKE = 2;
+    const static int MAX_HAIR_TIME = 600000;
     queue<int> chairs;   // a queue of chairs, the int value is the customer ID
     int MAX_CHAIRS;
+    int NUM_BARBERS;
     bool shop_state;  // false is closed, true is open
-    int barber_state;
+    status* barber_state;  //state of each barber
+    clock_t* barber_tosleep; //times when the barbers went to sleep
     
     pthread_mutex_t mutex;  // used for mutual exclusion of barbershop data
     pthread_cond_t cond_cust;  // used to synchronize the customers in the shop
-    pthread_cond_t cond_shop; // used to synchronize the barber
+    pthread_cond_t* cond_barber; // used to synchronize the barbers, this is an array
     
 public:
     barbershop();
+    void cleanup();
     
-    void open(int num_chairs);
+    void open(int num_chairs, int num_barbers);
     void close();
         
-    void perform_cuts();
+    void perform_cuts(int barber_id);
     
-    bool request_cut(int id);
+    bool request_cut(int cust_id);
     
 };
-
